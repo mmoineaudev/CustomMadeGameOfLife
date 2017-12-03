457a3c46db52a1d9e67e6c5b06c0a0260eb0b60e
Maxime M <maxime.moineau@free.fr>
2017-12-03 18:29:37 +0100

    On supprime l'inutile et on commence a penser aux interaction

--- app.js
+++ app.js
@@ -1,136 +1,85 @@
-// Bonne pratique : une fonction appelée quand la 
-// page est chargée
-window.onload = init;
 
-// Variables "globales"
-let canvas, ctx;
-let couleurTete = "orange";
-let xBonhomme = 0, yBonhomme = 100;
-let vitesseX = 10, vitesseY = 1;
 
-let bonhomme = {
-  couleurTete : "orange",
-  x: 0,
-  y: 100,
-  vitesseX : 10,
-  vitesseY : 1,
-  angleBrasgauche:0,
-  angleAvantBrasGauche: 0.5,
+//TODO still.... Utiliser au moins 3 ou 4 elements input de HTML5 (color, range, number par exemple) pour paramétrer votre jeu (vitesse, taille, nombre, couleur etc)
+
+
+window.onload=init;
+let canvas1, context, gamemap;
+
+function debug(f){
+  console.log("debug : "+f);
 }
 
-function init() {
-  console.log("page chargée");
-  
-  // On récupère dans le DOM le canvas (on ne peut
-  // le faire que si le DOM est "ready", c'est-
-  // à dire si la page est chargée)
-  // parametre = selecteur CSS
-  canvas = document.querySelector("#myCanvas");
-  
-  // on récupère le contexte graphique, qui a des
-  // propriétés et des méthodes pour dessiner
-  ctx = canvas.getContext("2d");
-  
-  // On lance l'animation
- animeBonhomme();
-  setInterval(changeCouleur, 1000);
+
+function init(){
+  //est-il possible d'utiliser le meme canvas pour un objet autre sans que l'anim du premier ne l'écrase  ? 
+  canvas1 = document.querySelector("#canvas1");
+
+  context = canvas1.getContext("2d");
+
+  gamemap = new GameMap(context, WIDTH, HEIGHT);
+  //bonhomme2 = new Bonhomme(100,100,canvas2,context2, 0.3, color1_b2, color2_b2, color3_b2);
+
+  animegamemap();
+
 }
 
-function changeCouleur() {
-  if(bonhomme.couleurTete === "orange") {
-    bonhomme.couleurTete = "green";
-  } else {
-    bonhomme.couleurTete = "orange";
+
+//comment ne pas faire de redondance ici ?
+function animegamemap(timeElapsed){
+    gamemap.contexte.clearRect(0, 0, gamemap.canvas.width, gamemap.canvas.height);
+  
+    gamemap.drawMap();
+
+    requestAnimationFrame(animegamemap);
+
+    
+};
+
+//fonctions utilitaires
+function accelerate_b1(){
+  //TODO
+  ;
+}
+function decelerate_b1(){
+  //TODO
+  ;
+}
+/**
+* maybe useful later
+*/
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
 }
 
-function animeBonhomme() {
-  // 1 - on efface le canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-  // 2 - on dessine dans le canvas
-  drawBonhomme(bonhomme.x, bonhomme.y);
-  
-  // 3 - on met à jour les objets à dessiner
-  bonhomme.angleBrasGauche += 0.1;
-  bonhomme.x += bonhomme.vitesseX;
-  if((bonhomme.x+100 >= canvas.width) || (bonhomme.x <= 0)){
-    bonhomme.vitesseX = -bonhomme.vitesseX;
+//Classes
+
+class FormOfLife{ //en ES6
+    
+    constructor(x, y, canvas, contexte){
+     debug("I am a form of life");
   }
   
-  bonhomme.y += bonhomme.vitesseY;
-  if((bonhomme.y+100 >= canvas.height) || (bonhomme.y-60 <= 0)){
-    bonhomme.vitesseY = -bonhomme.vitesseY;
+}
+/**
+* GameMap is meant to possess the bidimensionnal array and the drawLMap Method
+*/
+class GameMap{
+  
+  constructor(context, WIDTH, HEIGHT){
+    this.context=context;
+    this.WIDTH=WIDTH;
+    this.HEIGHT=HEIGHT;
+    debug("I am a map");
+
   }
-  
-  // 4 - On rapelle 60 fois par seconde la fonction
-  requestAnimationFrame(animeBonhomme);
-  
-}
-function drawBonhomme(x, y) {
-  // bonne pratique: sauver au début et restaurer le contexte à la fin
-  ctx.save();
-  
-  // x, y, largeur, hauteur. Origine = le coin
-  ctx.translate(x, y);
-  
-  // en haut à gauche
-  ctx.fillStyle = "red"; // valeur = une couleur CSS3
-  ctx.fillRect(0, 0, 100, 100);
-  
-  // Le cou et la tête
-  dessineCouEtTete();
-  
-  // Les bras
-  dessineBrasGauche();
-  
-  // deuxième bonne pratique: pour dessiner en x, y, on déplace
-  // le repère et on dessine comme si on était en 0, 0
-  ctx.restore();
-}
 
-function dessineCouEtTete() {
-  ctx.save();
-  
-  // On translate par rapport au repère courant (le corps)
-  ctx.translate(35, -20);
-    // Le cou
-  ctx.fillStyle = "blue";
-  ctx.fillRect(0, 0, 30, 20);
-  
-  // La tête, je translate par rapport au cou
-  ctx.translate(-10, -50);
-  ctx.fillStyle=bonhomme.couleurTete;
-  ctx.fillRect(0, 0, 50, 50)
-  
-  ctx.restore();
-}
-
-
-
-function dessineBrasGauche(angle) {
-  ctx.save();
-  
-  ctx.translate(0, 0);
-  ctx.rotate(bonhomme.angleBrasGauche);
-  
-  ctx.fillStyle="green";
-  ctx.fillRect(0, 0, 20, 50);
-  
-  // Dessin de l'avant bras gauche
-  dessineAvantBrasGauche(angle);
-  ctx.restore();
-}
-
-function dessineAvantBrasGauche(angle) {
-  ctx.save();
-  
-  ctx.translate(10, 40);
-  ctx.rotate(bonhomme.angleAvantBrasGauche);
-  // Pour changer le point de controle
-  ctx.translate(-10, 0);
-  
-  ctx.fillStyle="violet";
-  ctx.fillRect(0, 0, 20, 50);
-  ctx.restore();
+  function drawMap(){
+    //TODO
+  }
 }
