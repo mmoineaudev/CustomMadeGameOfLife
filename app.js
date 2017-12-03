3f99adfc430626c238298c7bac3931fa11cecc03
Maxime M <maxime.moineau@free.fr>
2017-12-03 19:00:19 +0100

    Fonction d'initialisation d'array multidimensionnel

--- app.js
+++ app.js
@@ -46,6 +46,31 @@ function decelerate_b1(){
   ;
 }
 /**
+* this is magic :
+* syntax : 
+* createArray();     // [] or new Array()
+* createArray(2);    // new Array(2)
+* createArray(3, 2); // [new Array(2),
+*                    //  new Array(2),
+*                    //  new Array(2)]
+
+*/
+function createArray(length) {
+//https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+
+
+/**
 * maybe useful later
 */
 function getRandomColor() {
